5c852d2f1fd4ba8bc0976df320c035a957756d02
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-07 17:29:29 -0400

    Change formatting on exec call

--- src/index.js
+++ src/index.js
@@ -12,18 +12,14 @@ const { exec } = require('@actions/exec');
     }
 
     const output = [];
-    await exec(
-      'git',
-      ['diff', '--name-only', github.context.payload.before, 'HEAD'],
-      {
-        silent: true,
-        listeners: {
-          stdout(data) {
-            output.push(data.toString());
-          },
+    await exec('git', ['diff', '--name-only', github.context.payload.before, 'HEAD'], {
+      silent: true,
+      listeners: {
+        stdout(data) {
+          output.push(data.toString());
         },
       },
-    );
+    });
     const text = output.join('').trim();
     core.setOutput('text', text);
     const json = text.split('\n');
