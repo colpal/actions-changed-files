dd2ebfe1a7342021518d2accf1adef60f597b97c
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-07 15:49:34 -0400

    Add basic error catch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-const { setOutput } = require('@actions/core');
+const core = require('@actions/core');
 const github = require('@actions/github');
 const { exec } = require('@actions/exec');
 
 (async () => {
-  const output = [];
-  await exec(
-    'git',
-    ['diff', '--name-only', github.event.before, 'HEAD'],
-    {
-      silent: true,
-      listeners: {
-        stdout(data) {
-          output.push(data.toString());
+  try {
+    const output = [];
+    await exec(
+      'git',
+      ['diff', '--name-only', github.event.before, 'HEAD'],
+      {
+        silent: true,
+        listeners: {
+          stdout(data) {
+            output.push(data.toString());
+          },
         },
       },
-    },
-  );
-  setOutput('files', output.join('').trim());
+    );
+    core.setOutput('files', output.join('').trim());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 })();
