08ee8420c86bce7805081381d5c9016daa84a235
Nicholas Farley <nicholas_farley@colpal.com>
2024-01-05 08:43:38 -0500

    Add deprecation message to 2.x series

--- dist/main.js
+++ dist/main.js
@@ -8705,6 +8705,7 @@ var core = require_core();
 var { getChanges, setTextOutputs } = require_lib4();
 (async () => {
   try {
+    core.warning('DEPRECATED: "colpal/actions-changed-files" is deprecated. For a suitable replacement, try "tj-actions/changed-files"');
     core.debug(JSON.stringify(github.context, null, 2));
     const json = await getChanges();
     core.setOutput("json", json);
